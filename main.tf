92849c7681e2d1930dd3ab1823315eae74efeeab
Santanu Das <santanu.das@littlehome.co.uk>
2023-04-01 13:20:51 +0000

    KB690 #comment Added New variable and Output value

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 ##
-resource "local_file" "file" {
-  content  = "moda"
-  filename = "${path.module}/moda.txt"
+variable "kb_item" {
+  type        = string
+  description = "Imported value"
+}
+
+resource "local_file" "this" {
+  content  = "This is content of moda"
+  filename = "${path.module}/${var.kb_item}_moda.txt"
+}
+
+output "file_name" {
+  value = split("/", local_file.this.filename)[1]
 }
